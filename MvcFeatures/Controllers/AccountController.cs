23146d1c4a27450019607fc2b3a6a01f6ee74c0c
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-20 14:51:40 +0300

    Add project files.

---
+++ MvcFeatures/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcFeatures.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
